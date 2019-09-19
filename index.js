c7c0ba2068452035ef127d44b914e61b57b8b8fd
Levi Miller <paullevimiller@gmail.com>
2019-09-19 13:39:39 -0700

    First three tests passed

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
+// Code below
 const fi = (function() {
   return {
     libraryMethod: function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, callback) {
+      const newCollection = (collection instanceof(Array)) ? 
+        newCollection.slice() : Object.values(collection);
 
+      for (let i = 0; i < newCollection.length; i++) {
+        callback(newCollection[i]);
+      };
+
+      return collection;
     },
 
-    map: function() {
+    map: function(collection, callback) {
+      const newCollection = (collection instanceof(Array)) ? 
+        newCollection.slice() : Object.values(collection);
 
+      for (let i = 0; i < newCollection.length; i++) {
+        newCollection[i] = callback(newCollection[i]);
+      };
+
+      return newCollection;
     },
 
-    reduce: function() {
+    reduce: function(collection, callback, acc) {
+      const newCollection = (collection instanceof(Array)) ? 
+        newCollection.slice() : Object.values(collection);
+
+      let newAcc = (acc instanceof(Number)) ? acc : 0;
+
+      for (let i = 0; i < newCollection.length; i++) {
+        newAcc += callback(newCollection[i]);
+      };
+
+      return newAcc;
 
     },
 
